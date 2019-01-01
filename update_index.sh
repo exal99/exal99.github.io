c553071ba06e21a68186080144abf8261c4dc1e1
Alexander Simko <alexander.ch.simko@gmail.com>
2019-01-01 19:27:37 +0100

    Added git calls again

--- update_index.sh
+++ update_index.sh
@@ -8,3 +8,10 @@ mv "src/Python/wikipedia-analysis/Wikipedia Statistics.html" exal99.github.io/in
 
 chmod a-x exal99.github.io/index.html
 
+cd exal99.github.io
+
+git add index.html
+
+git commit -m "Updated statistics"
+
+git push
