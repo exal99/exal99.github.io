b54fe2bf8b2ff9797fcfe20e0f5e53341cccdc74
Alexander Simko <alexander.ch.simko@gmail.com>
2019-01-04 11:30:00 +0100

    Added google analytics code to the build code

--- update_index.sh
+++ update_index.sh
@@ -10,4 +10,20 @@ chmod a-x exal99.github.io/index.html
 
 cd exal99.github.io
 
+sed -i '
+    1,5 {
+        /<head>/ {
+            r analytics.txt
+        }
+    }
+' index.html
+
 git add index.html
+
+if [ $# -eq 1 ]; then
+    git commit -m $1
+    git push
+elif [ $# -eq 0 ]; then
+    git commit -m "Updated statistics"
+    git push
+fi
